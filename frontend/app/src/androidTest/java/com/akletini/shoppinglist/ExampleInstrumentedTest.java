3b15a212b22da4458970c927191a4701132264e4
akletini <allen.kletinitch@smail.th-koeln.de>
2022-04-10 14:27:38 +0200

    Fix android project structure

--- frontend/app/src/androidTest/java/com/akletini/shoppinglist/ExampleInstrumentedTest.java
+++ frontend/app/src/androidTest/java/com/akletini/shoppinglist/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.shoppinglist;
+package com.akletini.shoppinglist;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.com.akletini.shoppinglist", appContext.getPackageName());
+        assertEquals("com.akletini.shoppinglist", appContext.getPackageName());
     }
 }
