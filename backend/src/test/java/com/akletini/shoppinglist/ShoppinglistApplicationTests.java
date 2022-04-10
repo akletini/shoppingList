debbf25f2a951ba9a1ca8fed0cb009a60735846b
akletini <allen.kletinitch@smail.th-koeln.de>
2022-04-10 15:16:11 +0200

    Add Spring initializr default project for backend

---
+++ backend/src/test/java/com/akletini/shoppinglist/ShoppinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akletini.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
